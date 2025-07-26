2db565966b82f4f7e43032eb10c1d291351db507
Lorenzo <lorenzodangel00@gmail.com>
2025-07-26 22:38:09 +0200

    added plugins.sbt

--- build.sbt
+++ build.sbt
@@ -8,5 +8,12 @@ lazy val root = (project in file("."))
       "dev.zio" %% "zio" % "2.1.19",
       "org.scala-lang.modules" %% "scala-swing" % "3.0.0",
       "org.scalatest" %% "scalatest" % "3.2.19" % Test
-    )
+    ),
+
+    assembly / mainClass := Some("controller.Simulation"),
+
+    assembly / assemblyMergeStrategy := {
+      case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+      case x => MergeStrategy.first
+    }
   )
---
+++ src/main/scala/controller/Simulation.scala
@@ -0,0 +1,42 @@
+package controller
+
+import model.World
+import model.managers.EcosystemManager
+import view.SimulationView
+import zio.Unsafe.unsafe
+import zio.{Ref, Runtime}
+
+/**
+ * Main entry point to run the Ecosystem Simulation application.
+ *
+ * <p>
+ * Initializes an empty world and creates the necessary components to run the simulation. 
+ * Sets up callbacks to update the view upon simulation state changes and opens the simulation window.
+ * </p>
+ *
+ * This function is annotated with '@main' and executed when the application starts.
+ */
+object Simulation:
+
+  def main(args: Array[String]): Unit =
+
+    val emptyWorld = World(0, 0, Seq.empty, Seq.empty, Seq.empty)
+
+    unsafe:
+      implicit u =>
+        val runtime = Runtime.default
+
+        val worldRef = runtime.unsafe.run(Ref.make(emptyWorld)).getOrThrowFiberFailure()
+        val ecosystemManager = new EcosystemManager(worldRef)
+        val stopFlag = Flag
+        val ecosystemController = new EcosystemController(ecosystemManager, stopFlag)
+        val simulationView = new SimulationView(ecosystemController)
+
+        ecosystemController.setUpdateViewCallback(() =>
+          simulationView.viewManager.updateView())
+
+        ecosystemController.setExtinctionCallback(() =>
+          simulationView.viewManager.updateView()
+          simulationView.viewManager.updateButtons())
+
+        simulationView.open()
